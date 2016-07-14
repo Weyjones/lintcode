233a6a7f556cf0ec1b3879ab1c1ae794853a6d31
Weizong Zhou <weyjones@gmail.com>
2016-07-13 21:40:52 -0700

    Jul.13.2016: balanced-binary-tree

---
+++ algorithm/chapter1/balanced-binary-tree.java
@@ -0,0 +1,40 @@
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+public class Solution {
+
+    /**http://www.lintcode.com/en/problem/balanced-binary-tree/
+     * @param root: The root of binary tree.
+     * @return: True if this Binary tree is Balanced, or false.
+     */
+    public boolean isBalanced(TreeNode root) {
+        // write your code here
+        if (root == null) {
+            return true;
+        }
+        
+        return getHeight(root) != -1;
+    }
+    
+    
+    public int getHeight(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftHeight = getHeight(root.left);
+        int rightHeight = getHeight(root.right);
+        
+        if (leftHeight == -1 || rightHeight == -1 || Math.abs(leftHeight - rightHeight) > 1) {
+            return -1;
+        }
+        return Math.max(leftHeight, rightHeight) + 1;
+    }
+}
